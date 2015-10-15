83de3809a5668c3b928936725065436c1822a51f
axis7818 <cameron.taylor7818@yahoo.com>
2015-10-15 16:21:37 -0700

    made the Word class only care about alphabetical characters.

--- STAT312WordAnalyzer/Word.cs
+++ STAT312WordAnalyzer/Word.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace STAT312WordAnalyzer
 {
@@ -8,14 +9,22 @@ namespace STAT312WordAnalyzer
     {
         private string _word;
 
-        private HashSet<char> chars = new HashSet<char>();
+        private HashSet<char> Chars = new HashSet<char>();
+
+        // matches a sequence of alphabetic characters
+        private static readonly Regex AlphaCheck = new Regex("[A-Za-z]+");
 
         public Word(string word)
         {
-            _word = word;
-            for(int i = 0; i < word.Length; i++)
+            // only use the alphabetical characters from the given word
+            _word = "";
+            foreach (Match m in AlphaCheck.Matches(word))
+                _word += m.Value;
+
+            // add the characters to the Chars HashSet
+            for(int i = 0; i < _word.Length; i++)
             {
-                chars.Add(_word[i]);
+                Chars.Add(_word[i]);
             }
         }
 
@@ -31,7 +40,7 @@ namespace STAT312WordAnalyzer
         {
             get
             {
-                return chars.Count;
+                return Chars.Count;
             }
         }
 
--- STAT312WordAnalyzer/WordAnalyzer.cs
+++ STAT312WordAnalyzer/WordAnalyzer.cs
@@ -7,27 +7,30 @@ namespace STAT312WordAnalyzer
 {
     public static class WordAnalyzer
     {
-        private static readonly string VowelString = "AEIOUaeiou";
+        private const string VowelString = "aeiou";
 
-        private static readonly HashSet<char> Vowels = new HashSet<char>() { 'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u' };
+        private static readonly HashSet<char> Vowels = new HashSet<char>() { 'a', 'e', 'i', 'o', 'u' };
 
-        private static readonly Regex YVowelCheck = new Regex("(?<![" + VowelString + "Yy])[Yy](?![" + VowelString + "])");
-
+        // Matches a 'y' that is not surrounded by other 'non-y' vowels
+        private static readonly Regex YVowelCheck = new Regex("(?<![" + VowelString + "])[y](?![" + VowelString + "])");
+        
         public static int VowelCount(Word word)
         {
+            // simplify the format of the word
+            string formattedWord = word.ToString().ToLower().Trim();
+
             // the number of vowels that are in the word
             int count = 0;
 
             // go through each letter and check for the simple vowels
-            foreach (char c in word)
+            foreach (char c in formattedWord)
             {
                 if (Vowels.Contains(c))
                     count++;
             }
 
             // check for 'y' vowels
-            MatchCollection yVowels = YVowelCheck.Matches(word);
-            count += yVowels.Count;
+            count += YVowelCheck.Matches(formattedWord).Count;
 
             // return the count
             return count;
@@ -35,7 +38,22 @@ namespace STAT312WordAnalyzer
 
         public static int ConsonantCount(Word word)
         {
+            // return the number of letters - the number that are vowels
             return word.Length - VowelCount(word);
+        }
+
+        public static int SyllableCount(Word word)
+        {
+            // get a lowercase/trimmed version of the string
+            string formattedWord = word.ToString().ToLower().Trim();
+
+            // the number of syllables in the word
+            int count = 0;
+
+
+
+            // return the result
+            return count;
         }
 
         public static float WordComplexity(Word word)
