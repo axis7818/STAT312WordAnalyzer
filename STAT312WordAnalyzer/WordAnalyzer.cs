cc716becf23ed80fb0d090f2c2562dcda4a07586
Cameron Taylor <cameron.taylor7818@yahoo.com>
2015-10-20 09:53:13 -0700

    added letter value dictionary

--- STAT312WordAnalyzer/WordAnalyzer.cs
+++ STAT312WordAnalyzer/WordAnalyzer.cs
@@ -7,6 +7,14 @@ namespace STAT312WordAnalyzer
 {
     public static class WordAnalyzer
     {
+        private static readonly Dictionary<char, int> LetterValues = new Dictionary<char, int>()
+        {
+            {'a', 1 }, {'b', 3 }, {'c', 3 }, {'d', 2 }, {'e', 1 }, {'f', 4 }, {'g', 2 }, {'h', 4 },
+            {'i', 1 }, {'j', 1 }, {'k', 5 }, {'l', 1 }, {'m', 3 }, {'n', 1 }, {'o', 1 }, {'p', 3 },
+            {'q', 10 }, {'r', 1 }, {'s', 1 }, {'t', 1 }, {'u', 1 }, {'v', 4 }, {'w', 4 }, {'x', 8 },
+            {'y', 4 }, {'z', 10 }
+        };
+
         private const string VowelString = "aeiou";
 
         private static readonly HashSet<char> Vowels = new HashSet<char>() { 'a', 'e', 'i', 'o', 'u' };
