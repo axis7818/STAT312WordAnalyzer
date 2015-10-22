a1c1ca8a518bfac494688b7df0f287747727e673
axis7818 <cameron.taylor7818@yahoo.com>
2015-10-21 22:08:46 -0700

    initial create of GUI

---
+++ WordAnalyzerGUI/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace WordAnalyzerGUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_GetSample_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(this, "Not yet implemented!", "Not Implemented", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void MI_Options_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(this, "Not yet implemented!", "Not Implemented", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void MI_OpenResultsFile_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(this, "Not yet implemented!", "Not Implemented", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
