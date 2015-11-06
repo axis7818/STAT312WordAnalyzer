08102a5faa8b502131ac9442b537cd307ef34beb
Cameron Taylor <cameron.taylor7818@yahoo.com>
2015-11-06 11:51:41 -0800

    moved data management to Stat312WordAnalizer.dll

--- STAT312WordAnalyzer/DataFileManager.cs
+++ STAT312WordAnalyzer/DataFileManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Xml.Serialization;
 using STAT312WordAnalyzer;
 
 namespace WordAnalyzerGUI
